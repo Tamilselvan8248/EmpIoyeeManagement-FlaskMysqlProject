65c593716be1913620300af4abc8a0c7164cb907
Tamilselvan <147641317+Tamilselvan8248@users.noreply.github.com>
2024-11-07 09:56:09 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@ CREATE TABLE empdetails
 	id VARCHAR(10) primary key,
    name VARCHAR(45) not null,
   email VARCHAR(30) not null,
-  phone int(10) not null
+  phone VARCHAR (10) not null
   );
 insert into empdetails values('e001','Raj','raj@gmail.com',9750037022);
 insert into empdetails values('e002','Sakthi','sakthi@gmail.com',9712345678);
